1ce29f21d4cdeaee85ae8e18900d873846db7862
184935 <184935@stud.hvl.no>
2026-04-27 10:18:09 +0200

    Views and controllers
    Lagt til flere views og controller til disse

---
+++ CaseSetup/Controllers/CaseBuilderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaseSetup.Controllers
+{
+    public class CaseBuilderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Student()
+        {
+            return View();
+        }
+        public IActionResult Teacher()
+        {
+            return View();
+        }
+        public IActionResult AddUserData()
+        {
+            return View();
+        }
+    }
+}
--- CaseSetup/Controllers/HomeController.cs
+++ CaseSetup/Controllers/HomeController.cs
@@ -15,11 +15,21 @@ namespace CaseSetup.Controllers
         {
             return View();
         }
+        public IActionResult Student()
+        {
+            return View();
+        }
+        public IActionResult Teacher()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
