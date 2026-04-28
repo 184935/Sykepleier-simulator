48db91da7dec04178d6c7ab7f89335dc4e8829bd
184935 <184935@stud.hvl.no>
2026-04-28 13:15:24 +0200

    Lagt til nødvendig DBSet

--- CaseSetup/Data/ApplicationDbContext.cs
+++ CaseSetup/Data/ApplicationDbContext.cs
@@ -7,5 +7,12 @@ namespace CaseSetup.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
         public DbSet<SharedLibrary.Models.Case> Case { get; set; } = default!;
+        public DbSet<SharedLibrary.Models.Allergy> Allergies { get; set; } = default!;
+        public DbSet<SharedLibrary.Models.Diagnosis> Diagnosis { get; set; } = default!;
+        public DbSet<SharedLibrary.Models.LabValues> LabValues {  get; set; } = default!;
+        public DbSet<SharedLibrary.Models.MedicalHistory> MedHistory {  get; set; } = default!;
+        public DbSet<SharedLibrary.Models.Medication> Medications {  get; set; } = default!;
+        public DbSet<SharedLibrary.Models.Patient> Patients {  get; set; } = default!;
+        public DbSet<SharedLibrary.Models.Vitals> Vitals {  get; set; } = default!;
     }
 }
