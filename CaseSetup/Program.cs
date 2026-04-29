7d73e86da94dd31a83d8ee5b0e7594928521386d
184935 <184935@stud.hvl.no>
2026-04-29 16:09:15 +0200

    Fikset login
    Kan nå logge inn med opprettet bruker

--- CaseSetup/Program.cs
+++ CaseSetup/Program.cs
@@ -18,7 +18,7 @@ namespace CaseSetup
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
