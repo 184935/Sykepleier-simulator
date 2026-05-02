dc3f300ee6134807cbd3480f1d06fa39ca2af739
184935 <184935@stud.hvl.no>
2026-05-02 12:23:06 +0200

    Opprettet rest api
    Opprettet default rest-api prosjekt

---
+++ Rest-API/Controllers/CaseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rest_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaseController : ControllerBase
+    {
+        // GET: api/<CaseController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CaseController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CaseController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CaseController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CaseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
